37ca41bc97872ebe212db7ac8c2cda1a5027e2a2
Frank Giger <frank.giger97@bluewin.ch>
2017-04-30 00:45:13 +0200

    Add more tests.

--- tests/Types/DirectionsTest.php
+++ tests/Types/DirectionsTest.php
@@ -1,7 +1,26 @@
 <?php
 namespace Frolki1_Dev\Google\Embed\Tests\Types;
 
+use Frolki1_Dev\Types\Directions;
+
 class DirectionsTest extends TypeTestCase
 {
+    /**
+     * @var Directions
+     */
+    protected $direction;
 
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->direction = $this->googleMaps->createDirectionsMap();
+    }
+
+    public function testOriginParameter()
+    {
+        $this->direction->start($this->location);
+
+        $this->assertEquals($this->location, $this->direction->getParameter('origin'));
+    }
 }
